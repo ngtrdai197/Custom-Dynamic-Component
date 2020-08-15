db61f3f4f32f2233d59b8c075ce82aa84c230fe4
Dai Nguyen <ngtrdai290197@gmail.com>
2020-08-15 18:17:09 +0700

    (feat): update logics at parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentRef, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators'
+import { MyCommunityComponent } from './my-community/my-community.component';
+import { MyOptionsComponent } from './my-options/my-options.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Dynamic-Component';
+export class AppComponent implements OnInit {
+  public title = 'App component ...'
+  @ViewChild('container', { read: ViewContainerRef, static: true }) containerRef: ViewContainerRef
+  private ngDestroyed$ = new Subject<boolean>()
+  private myCommunityRef: ComponentRef<MyCommunityComponent>
+  private myOptionsRef: ComponentRef<MyOptionsComponent>
+  constructor(private readonly cfr: ComponentFactoryResolver) { }
+
+  ngOnInit() {
+    this.onActivateMyCommunity()
+    this.handleMyCommunity()
+    this.handleMyOptions()
+  }
+
+  public async onActivateMyCommunity() {
+    this.clearContainerRef()
+    const { MyCommunityComponent } = await import('./my-community/my-community.component')
+    const injector = this.containerRef.injector
+    const componentFactory = this.cfr.resolveComponentFactory(MyCommunityComponent)
+    const componentRef = this.containerRef.createComponent(componentFactory, this.containerRef.length, injector);
+    this.myCommunityRef = componentRef
+    componentRef.instance.name = 'My Community'
+    const cb = componentRef.instance.nameChange
+    this.handleMyCommunity(cb)
+  }
+
+  public async onActivateMyOptions() {
+    this.clearContainerRef()
+    const { MyOptionsComponent } = await import('./my-options/my-options.component')
+    const componentFactory = this.cfr.resolveComponentFactory(MyOptionsComponent)
+    const componentRef = this.containerRef.createComponent(componentFactory);
+    this.myOptionsRef = componentRef
+    const cb = componentRef.instance.nameChange
+    this.handleMyOptions(cb)
+  }
+
+
+  private handleMyCommunity(cb?: EventEmitter<string>) {
+    if (!cb) return
+    this.myCommunityChangeName(cb)
+  }
+
+  private myCommunityChangeName(cb?: EventEmitter<string>) {
+    cb.pipe(
+      takeUntil(this.ngDestroyed$)
+    ).subscribe((newName: string) => {
+      this.myCommunityRef.instance.name = newName
+      this.title = newName
+    })
+  }
+
+  private handleMyOptions(cb?: EventEmitter<string>) {
+    if (!cb) return
+    this.myOptionsChangeName(cb)
+  }
+
+  private myOptionsChangeName(cb?: EventEmitter<string>) {
+    cb.pipe(
+      takeUntil(this.ngDestroyed$)
+    ).subscribe((newName: string) => {
+      this.myOptionsRef.instance.name = newName
+      this.title = newName
+    })
+  }
+
+  private clearContainerRef() {
+    this.containerRef.clear()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyCommunityComponent } from './my-community/my-community.component';
+import { MyOptionsComponent } from './my-options/my-options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCommunityComponent,
+    MyOptionsComponent
   ],
   imports: [
     BrowserModule
